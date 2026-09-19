50218562710efdff8439f7e8b45720f1f1a20fba
myacoubi <amineamoune904@gmail.com>
2026-09-19 22:00:59 +0100

    feat(cart): initialize cart microservice

---
+++ backend/cart-service/src/test/java/com/buy02/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buy02.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
